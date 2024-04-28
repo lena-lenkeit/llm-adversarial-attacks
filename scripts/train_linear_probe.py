ab89f237d5a67fa22f778356c753abde4c63ce4a
lena-lenkeit <92172030+lena-lenkeit@users.noreply.github.com>
2024-04-29 00:49:10 +0200

    Expand dataset with activations

--- scripts/train_linear_probe.py
+++ scripts/train_linear_probe.py
@@ -1,6 +1,11 @@
 import pathlib
 
 import datasets
+import numpy as np
+import torch
+from tqdm.auto import tqdm as tq
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers.modeling_outputs import CausalLMOutputWithPast
 
 
 def make_imdb_sentiment_dataset(dataset_root_dir: str):
@@ -45,9 +50,66 @@ def make_imdb_sentiment_dataset(dataset_root_dir: str):
     return dataset
 
 
+@torch.no_grad()
 def main():
-    dataset = make_imdb_sentiment_dataset("data/aclImdb")
+    # Parameters
+    max_elements = 256
+    max_length = 512
+    device = "cuda"
+
+    # Directories
+    model_path = "EleutherAI/pythia-70m"
+    dataset_path = "data/aclImdb"
+    activation_cache_path = "activations/pythia-70m-aclimdb"
+
+    # Load model, tokenizer and dataset
+    model = AutoModelForCausalLM.from_pretrained(
+        model_path, device_map=device, torch_dtype=torch.float32
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    dataset = make_imdb_sentiment_dataset(dataset_path)
+
+    # Limit dataset length
+    for split in dataset:
+        dataset[split] = dataset[split].select(range(max_elements), keep_in_memory=True)
+
+    # Print info on model, tokenizer and dataset
+    print("---MODEL---")
+    print(model)
+    print("---TOKENIZER---")
+    print(tokenizer)
+    print("---DATASET---")
     print(dataset)
+
+    # Run entire dataset through model, caching all hidden layer activations at the last
+    # token
+    for split in dataset:
+        activation_cache = {}
+        for row in tq(dataset[split]):
+            text: str = row["text"]
+
+            tokens = tokenizer(
+                text, max_length=max_length, truncation=True, return_tensors="pt"
+            )
+            tokens = tokens.to(device)
+
+            with torch.no_grad():
+                outputs: CausalLMOutputWithPast = model(
+                    **tokens, output_hidden_states=True
+                )
+
+            for cache_id, hidden_state in enumerate(outputs.hidden_states):
+                cache = activation_cache.get(cache_id, [])
+                cache.append(hidden_state[0, -1].cpu().numpy())
+                activation_cache[cache_id] = cache
+
+        for cache_id in activation_cache:
+            dataset[split] = dataset[split].add_column(
+                f"hidden_{cache_id}", activation_cache[cache_id]
+            )
+
+    # Save dataset, with activations included
+    dataset.save_to_disk(activation_cache_path)
 
 
 if __name__ == "__main__":
