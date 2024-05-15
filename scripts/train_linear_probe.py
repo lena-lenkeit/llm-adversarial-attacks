8146fb523e2b7302bdcde5319833c75deb2c6d1d
lena-lenkeit <92172030+lena-lenkeit@users.noreply.github.com>
2024-05-15 21:55:36 +0200

    Added argpase to probe training script

--- scripts/train_linear_probe.py
+++ scripts/train_linear_probe.py
@@ -1,6 +1,6 @@
+import argparse
 import os
 
-import datasets
 import matplotlib.pyplot as plt
 import numpy as np
 import safetensors
@@ -9,6 +9,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import RocCurveDisplay, roc_auc_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+
+import datasets
 
 
 def prepare_data(dataset: datasets.Dataset, layer_id: int):
@@ -21,10 +23,10 @@ def prepare_data(dataset: datasets.Dataset, layer_id: int):
     return features, labels
 
 
-def main():
+def main(args: argparse.Namespace):
     # Directories
-    activation_path = "activations/pythia-70m-aclimdb"
-    probe_path = "probes/pythia-70m-aclimdb"
+    activation_path = args.activation_path
+    probe_path = args.probe_path
 
     os.makedirs(probe_path, exist_ok=True)
 
@@ -32,24 +34,19 @@ def main():
     dataset = datasets.load_from_disk(activation_path)
 
     # Prepare data
-    layer_id = 6
+    layer_id = args.layer_id
 
     features_train, labels_train = prepare_data(dataset["train"], layer_id)
     features_test, labels_test = prepare_data(dataset["test"], layer_id)
 
     # Train probe
     scaler = StandardScaler()
-    probe = LogisticRegression(max_iter=100000)
+    probe = LogisticRegression(max_iter=args.max_iter)
     pipe = make_pipeline(scaler, probe)
 
     pipe = pipe.fit(features_train, labels_train)
 
     # Convert to linear layer
-    # print(probe.coef_.shape)  # (num_classes, num_features)
-    # print(probe.intercept_.shape)  # (num_classes,)
-    # print(scaler.scale_.shape)  # (num_features,)
-    # print(scaler.mean_.shape)  # (num_features,)
-
     weight = probe.coef_ / scaler.scale_[None]  # (num_classes, num_features)
     bias = probe.intercept_ - (scaler.mean_[None] @ weight.T)[0]  # (num_classes,)
 
@@ -65,16 +62,18 @@ def main():
 
     # Test probe
     roc_auc_train = roc_auc_score(labels_train, scores_linear_train)
-    RocCurveDisplay.from_predictions(labels_test, scores_linear_test)
-    plt.savefig(f"{probe_path}/roc_train.png", bbox_inches="tight")
-    plt.savefig(f"{probe_path}/roc_train.svg", bbox_inches="tight")
-    plt.close()
-
     roc_auc_test = roc_auc_score(labels_test, scores_linear_test)
-    RocCurveDisplay.from_predictions(labels_test, scores_linear_test)
-    plt.savefig(f"{probe_path}/roc_test.png", bbox_inches="tight")
-    plt.savefig(f"{probe_path}/roc_test.svg", bbox_inches="tight")
-    plt.close()
+
+    if args.plot:
+        RocCurveDisplay.from_predictions(labels_test, scores_linear_test)
+        plt.savefig(f"{probe_path}/roc_train.png", bbox_inches="tight")
+        plt.savefig(f"{probe_path}/roc_train.svg", bbox_inches="tight")
+        plt.close()
+
+        RocCurveDisplay.from_predictions(labels_test, scores_linear_test)
+        plt.savefig(f"{probe_path}/roc_test.png", bbox_inches="tight")
+        plt.savefig(f"{probe_path}/roc_test.svg", bbox_inches="tight")
+        plt.close()
 
     # Save probe
     safetensors.numpy.save_file(
@@ -90,4 +89,37 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Train and evaluate a probe.")
+    parser.add_argument(
+        "--activation_path",
+        type=str,
+        default="activations/pythia-160m-coherence",
+        help="Path to the activation dataset.",
+    )
+    parser.add_argument(
+        "--probe_path",
+        type=str,
+        default="probes/pythia-160m-coherence",
+        help="Path to save the probe to.",
+    )
+    parser.add_argument(
+        "--layer_id",
+        type=int,
+        default=None,
+        help="Hidden layer ID to extract features from.",
+    )
+    parser.add_argument(
+        "--max_iter",
+        type=int,
+        default=1000000,
+        help="Maximum number of iterations for the logistic regression to converge.",
+    )
+    parser.add_argument(
+        "-p",
+        "--plot",
+        action="store_true",
+        help="Set to plot ROC curves.",
+    )
+
+    args = parser.parse_args()
+    main(args)
